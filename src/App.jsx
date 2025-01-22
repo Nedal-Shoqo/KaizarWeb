4572cac438acae0997e881f3bfa0529043caadd4
NedalShoqo <s12217300@stu.najah.edu>
2025-01-22 18:09:50 +0200

    front

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     
+      <div className="LandingPageBox1">
+
+
+        <div className="RightSideBox1">
+        <img src="src/assets/Pics/LandingPage/Rectangle.png" alt="Landing Page Rectangle" />
+        </div>
+
+        <div className="LeftSideBox1">
+          <button> </button>
+        </div>
+
+        
+      </div>
+
+      <div className="LandingPageBox2"></div>
+      <div className="LandingPageBox1"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
