34db2f6a8b5f871dd5a76acf049637d56ab192c5
NedalShoqo <s12217300@stu.najah.edu>
2025-01-23 18:16:20 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,28 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="MainDiv">
       <div className="LandingPageBox1">
 
 
         <div className="RightSideBox1">
-        <img src="src/assets/Pics/LandingPage/Rectangle.png" alt="Landing Page Rectangle" />
+        <img id="Man" src="src/assets/Pics/LandingPage/Group1.png" alt="Landing Page Rectangle" />
+\
         </div>
 
         <div className="LeftSideBox1">
-          <button> </button>
+<h6> Men Section </h6>
+          <h1> Arrived Now </h1>
+          <h3> Latest Man Trends </h3>
+          <button id="ShopNow"> Shop Now </button>
         </div>
 
         
       </div>
 
-      <div className="LandingPageBox2"></div>
-      <div className="LandingPageBox1"></div>
-    </>
+      <div className="LandingPageBox2"> 2  </div>
+      <div className="LandingPageBox1"> 3 </div>
+    </div>
   );
 }
 
