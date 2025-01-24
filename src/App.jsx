42f1e183999afae16259f94604b507e844d5bda5
NedalShoqo <s12217300@stu.najah.edu>
2025-01-24 11:42:19 +0200

    Added Decorations

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ function App() {
     <div className="MainDiv">
       <div className="LandingPageBox1">
         <div className="LeftSideBox1">
-          <h6> Men Section </h6>
+
+
+          <div className="MenSectionText">
+          <p id="_2"> ___ </p>
+            <h6> Men Section </h6>
+            <p id="_"> ___ </p>
+          </div>
+
+<div className="ArrivedNowDecoration">
+  <p id="Line"> | </p>
           <h1> Arrived Now </h1>
+           
+</div>
           <h3> Latest Man Trends </h3>
           <button id="ShopNow"> Shop Now </button>
         </div>
@@ -20,32 +31,26 @@ function App() {
         </div>
       </div>
 
-      <div className="LandingPageBox2"> 
-        <div className="MenAndWoman"> 
+      <div className="LandingPageBox2">
+        <div className="MenAndWoman">
+          <div id="Men">
+            <p id="MenText"> Discover The Mans World </p>
+            <button id="MenButton"> View Now!</button>
+          </div>
 
-
-         
-        <div id="Men">
-         <p id="MenText"> Discover The Mans World </p>
-      <button id="MenButton"> View Now!</button>
-
+          <div id="Woman">
+            <p id="WomanText"> Explore The Womens Fashion </p>
+            <button id="WomanButton"> Join Now!</button>
+          </div>
         </div>
 
-        <div id="Woman" >
-        <p id="WomanText"> Explore The Womens Fashion </p>
-        <button id="WomanButton"> Join Now!</button>
-        </div>
-        </div>
-        
-        <p id="CategoryText"> Be the first to know about 
-        upcoming sales and special clothes. </p>
-         </div>
+        <p id="CategoryText">
+          {" "}
+          Be the first to know about upcoming sales and special clothes.{" "}
+        </p>
+      </div>
 
-
-
-      <div className="LandingPageBox3"> 
-        
-        </div>
+      <div className="LandingPageBox3"></div>
     </div>
   );
 }
